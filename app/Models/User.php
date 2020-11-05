5384f50fcf5c0ca7a66e3ee87457a290c0293a26
Shek Muktar <im.skm07@gmail.com>
2020-11-05 16:37:46 +0530

    User: Model: Added changePassword method

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Fantom\Database\Model;
+use App\Support\Traits\LastErrorTrait;
 
 /**
  * User model
@@ -11,6 +12,8 @@ class User extends Model
 {
 	protected $primary = 'id';
 	protected $table   = 'users';
+
+	use LastErrorTrait;
 
 	public static function make(array $data)
 	{
@@ -22,5 +25,22 @@ class User extends Model
 		$user->password 	= password_hash(trim($data['password']), PASSWORD_DEFAULT);
 
 		return $user;
+	}
+
+	public function changePassword($old_password, $new_password)
+	{
+		if (! password_verify($old_password, $this->password)) {
+			$this->setError("Old password doesn't match in database.");
+			return false;
+		}
+
+		$this->password = password_hash($new_password, PASSWORD_DEFAULT);
+		
+		if ($this->save() === false) {
+			$this->setError("Failed to update new password.");
+			return false;
+		}
+
+		return true;
 	}
 }
