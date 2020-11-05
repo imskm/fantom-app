20a6299b7b2588e6dc20e1f2e27a7036724d016c
Shek Muktar <im.skm07@gmail.com>
2020-11-05 16:36:03 +0530

    LastErrorTrait: Trait: Added last error fetching trait
    This is useful when a method returns false with some error and we
    want to know what was the actual error then we can use it.
    Surely it's not the way OOP works, better solution is to use Exception.

---
+++ app/Support/Traits/LastErrorTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Support\Traits;
+
+/**
+ * This tait is helpful when an object performs action and need to provide
+ * caller some way to know exact error message.
+ * It only keeps track of last error just like in C errno API.
+ */
+trait LastErrorTrait
+{
+	/**
+	 * Naming of instance variable is done this way to avoid collision by
+	 * actual class that also has the error variable.
+	 * Naming this way will avoid double declaration fatal error
+	 */
+	private $_last_error = "";
+
+	public function setError($message)
+	{
+		$this->_last_error = $message;
+	}
+
+	public function getError()
+	{
+		return $this->_last_error;
+	}
+}
