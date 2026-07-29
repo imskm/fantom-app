d676d425cbb872b0a4971f7e115cee98a5a08b6a
Shek Muktar <im.skm07@gmail.com>
2026-07-29 12:23:27 +0530

    composer: Create a `setup` composer script command that create assets
    and Config.php file

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Fantom Application
 A web application based on my own PHP MVC framework **Fantom**
 This fantom app is a basic setup of fantom app
+
+
+# After project setup
+
+Run the composer script `setup` to create necessary files and Folders
+
+```console
+composer setup
+```
---
+++ scripts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p public/assets/css
+mkdir -p public/assets/img
+mkdir -p public/assets/js
+mkdir -p public/uploads
+
+chmod 777 public/uploads
+
+if [ ! -f app/Config.php ]; then
+	cp app/Config.example.php app/Config.php
+else
+	echo "Config.php file already exists."
+fi
